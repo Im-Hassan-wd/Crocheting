d76b31f659b4a3e60d7bc01cad5efee1c776dfa8
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-26 21:27:23 +0100

    handling errors

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,10 +6,12 @@ const handleErrors = (err) => {
 
   // validation errors
   if(err.message.includes('user validation failed')){
-    Object.values(err.errors).forEach(error => {
-      console.log(error.properties);
+    Object.values(err.errors).forEach(({properties}) => {
+      errors[properties.path] = properties.message;
     });
   }
+
+  return errors;
 }
 
 module.exports.signup_get = (req, res) => {
@@ -29,7 +31,7 @@ module.exports.signup_post = async (req, res) => {
   }
   catch(err) {
     const errors = handleErrors(err);
-    res.status(400).send('user not created');
+    res.status(400).json({ errors });
   }
 }
 
