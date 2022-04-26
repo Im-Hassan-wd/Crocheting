f4b33e9247e7a4f988a4caec60f1300e4ae92be9
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-26 21:10:29 +0100

    mongoose validation

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,15 @@
 const User = require('../models/User');
 
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
+
+  // validation errors
+  if(err.message.include('user validation failed')){
+    console.log(err);
+  }
+}
+
 module.exports.signup_get = (req, res) => {
   res.render('signup', { title: "signup" });
 }
@@ -16,7 +26,7 @@ module.exports.signup_post = async (req, res) => {
     res.status(201).json(user);
   }
   catch(err) {
-    console.log(err);
+    const errors = handleErrors(err);
     res.status(400).send('user not created');
   }
 }
