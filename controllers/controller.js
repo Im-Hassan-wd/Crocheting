ac8bf3719714595d480dad3fcfb5f748ffb3ead1
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-26 22:37:55 +0100

    signing users

--- controllers/controller.js
+++ controllers/controller.js
@@ -30,8 +30,6 @@ module.exports.login_get = (req, res) => {
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password)
-
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user);
