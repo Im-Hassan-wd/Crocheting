e7ae6cec90c5768c16921c76bc83cad85584e1c6
Eunice Oyeniyi <alhassansahih@gmail.com>
2022-04-26 15:02:30 +0100

    model validations

--- models/User.js
+++ models/User.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
+const { email } = require('validator');
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter an email'],
     unique: true,
-    lowercase: true
+    lowercase: true,
+    validate: [email, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, 'Please enter a paasword'],
+    minlength: [6, 'Minimum paasword length is 6 characters']
   }
 });
 
